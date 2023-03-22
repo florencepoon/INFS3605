a3fb3f94f6ae9a4bddac896a78a6e1f723bab88d
florencepoon <106735415+florencepoon@users.noreply.github.com>
2023-03-22 12:27:35 +1100

    landing page -> sign in COMPLETE

---
+++ app/src/main/java/com/example/infs3605/LandingPage.java
@@ -0,0 +1,22 @@
+package com.example.infs3605;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+
+    public void login (View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
