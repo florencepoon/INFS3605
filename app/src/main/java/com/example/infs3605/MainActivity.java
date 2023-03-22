1640b53ca982da49dd7830fed6f47f402ecd9330
florencepoon <106735415+florencepoon@users.noreply.github.com>
2023-03-22 20:24:32 +1100

    linking nav bar ver2.0
    cannot fkin do it gdi

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -1,15 +1,55 @@
 package com.example.infs3605;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainActivityBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_dashboard);
+
+        binding = MainActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.Home:
+                    replaceFragment();
+                    break;
+
+                case R.id.Profile:
+                    break;
+
+                case R.id.Insights:
+                    break;
+
+                case R.id.Settings:
+                    break;
+
+                case R.id.Dashboard:
+                    break;
+
+            }
+
+            return true;
+        });
+
+        private void replaceFragment(Fragment fragment){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frameLayout,fragment);
+            fragmentTransaction.commit();
+        }
 
     }
 }
