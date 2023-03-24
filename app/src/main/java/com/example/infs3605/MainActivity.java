0aac83e865e0e9e6f3f62c1467a422f250d748a9
BryanTheLad <106734213+BryanTheLad@users.noreply.github.com>
2023-03-24 23:18:00 +1100

    Revert "Merge branch 'main' of https://github.com/florencepoon/INFS3605"
    This reverts commit 741f3cf4feb73c7b51a4a584a6a9b2c05e3d2bd3, reversing
    changes made to 69cb093c19ee2ab1604892df3ce4a70fba995fe0.

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -1,53 +1,58 @@
 package com.example.infs3605;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-
+import com.example.infs3605.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
-
-    Home home = new Home();
-    Insights insights = new Insights();
-    Profile profile = new Profile();
+    ActivityMainBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,home).commit();
-
-        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected( MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.Home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,home).commit();
-                        return true;
-                    case R.id.insights:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,insights).commit();
-                        return true;
-                    case R.id.profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,profile).commit();
-                        return true;
-                }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new Home());
 
         BottomNavigationView mBottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         mBottomNavigationView.getMenu().findItem(R.id.Home).setChecked(true);
 
-                return false;
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.Home:
+                    replaceFragment(new Home());
+                    break;
+
+                case R.id.Profile:
+                    replaceFragment(new Profile());
+                    break;
+
+                case R.id.Insights:
+                    replaceFragment(new Insights());
+                    break;
+
             }
+
+            return true;
         });
     }
+
+        private void replaceFragment(Fragment fragment){
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.frameLayout,fragment);
+            fragmentTransaction.commit();
+        }
+
 }
+
 
 
