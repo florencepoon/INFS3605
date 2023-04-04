71d20464a458a54fc8f71a94133bb15038fc19b1
florencepoon <106735415+florencepoon@users.noreply.github.com>
2023-04-04 17:27:05 +1000

    event detail functionality ver1.0

--- app/src/main/java/com/example/infs3605/EventsDetail.java
+++ app/src/main/java/com/example/infs3605/EventsDetail.java
@@ -1,5 +1,6 @@
 package com.example.infs3605;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class EventsDetail extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_events_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_events_detail, container, false);
+
+        //Delete event button
+        ImageView deleteEvent = view.findViewById(R.id.deleteEventButton);
+        deleteEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //Edit event button
+        ImageView editEvent = view.findViewById(R.id.editEventButton);
+        editEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
