dfb4565a4afb6d74cbc0c9c4b4863e282336a4f8
florencepoon <106735415+florencepoon@users.noreply.github.com>
2023-04-08 23:18:35 +1000

    event detail linking information to database

--- app/src/main/java/com/example/infs3605/EventsDetail.java
+++ app/src/main/java/com/example/infs3605/EventsDetail.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,7 +21,10 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 public class EventsDetail extends Fragment {
-
+    private ValueEventListener eventListener;
+    private TextView eventNameText, eventCategoryText, eventParticipationText, eventOrganiserText, eventLocationText, eventStartTimeText, eventDateText;
+    private String eventId = getArguments().getString("eventId");
+    DatabaseReference eventRef = FirebaseDatabase.getInstance().getReference().child("events").child(eventId);
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -28,6 +32,33 @@ public class EventsDetail extends Fragment {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference eventsRef = database.getReference("events");
+
+        eventNameText = view.findViewById(R.id.eventsNameDetail);
+        eventCategoryText = view.findViewById(R.id.eventsCategoryDetail);
+        eventParticipationText = view.findViewById(R.id.eventsParticipationDetail);
+        eventOrganiserText = view.findViewById(R.id.EventsOrganiserDetail);
+        eventLocationText = view.findViewById(R.id.eventsLocationDetail);
+        eventDateText = view.findViewById(R.id.eventsDateTimeDetail);
+        eventStartTimeText = view.findViewById(R.id.eventsDateTimeDetail);
+
+        // Attach a value event listener to the event node to populate the UI with data
+        eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Event event = dataSnapshot.getValue(Event.class);
+                    eventNameText.setText(event.getEventName());
+                    eventLocationText.setText(event.getEventLocation());
+                    eventCategoryText.setText(event.getEventCategory());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+            }
+        };
+        eventRef.addValueEventListener(eventListener);
 
         //Delete event button
         ImageView deleteEvent = view.findViewById(R.id.deleteEventButton);
@@ -64,5 +95,12 @@ public class EventsDetail extends Fragment {
         });
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        // Remove the value event listener when the fragment is destroyed
+        eventRef.removeEventListener(eventListener);
     }
 }
